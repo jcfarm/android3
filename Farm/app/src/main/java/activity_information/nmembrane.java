6065678dc29404f43b8794e5b913d4aab95b89fb
yhf <yhf@yhfdeAir.lan>
2017-06-01 23:24:49 +0800

    yhf

---
+++ Farm/app/src/main/java/activity_information/nmembrane.java
@@ -0,0 +1,23 @@
+package activity_information;
+
+/**
+ * Created by yhf on 2017/6/1.
+ */
+
+public class nmembrane {
+    private int id;
+    private float value;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public float getValue() {
+        return value;
+    }
+    public void setValue(float value) {
+        this.value = value;
+    }
+
+}
---
+++ Farm/app/src/main/java/activity_information/salt.java
@@ -0,0 +1,30 @@
+package activity_information;
+
+/**
+ * Created by yhf on 2017/6/1.
+ */
+
+public class salt {
+    private int id;
+    private float us;
+    private float mg;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public float getUs() {
+        return us;
+    }
+    public void setUs(float us) {
+        this.us = us;
+    }
+    public float getMg() {
+        return mg;
+    }
+    public void setMg(float mg) {
+        this.mg = mg;
+    }
+
+}
